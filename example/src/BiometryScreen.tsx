a2690032205d86d9691982989220e3f2d902c8a9
Yagmur Akincisoylu <yagmurakincisoylu@gmail.com>
2021-12-09 15:22:52 +0100

    add biometry screen to example project

---
+++ example/src/BiometryScreen.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+
+import { SafeAreaView, ScrollView, View, Text, Button } from 'react-native';
+import DeviceCrypto from 'react-native-device-crypto';
+import styles from './styles';
+
+const BiometryScreen = () => {
+  const [error, setError] = React.useState<string | undefined>('');
+  const [isAuthenticated, setIsAuthenticated] = React.useState<boolean>();
+
+  const simpleAuthentication = async () => {
+    try {
+      const res = await DeviceCrypto.authenticateWithBiometry({
+        confirmationRequired: true,
+        biometryDescription: 'Description',
+        biometrySubTitle: 'Sub title',
+        biometryTitle: ' Title',
+      });
+      setIsAuthenticated(res);
+      setError('');
+    } catch (err: any) {
+      setError(err.message);
+      setIsAuthenticated(false);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Text>Confirm with biometry</Text>
+        <Button
+          onPress={simpleAuthentication}
+          title="Fire Biometric Authentication"
+          color="#841584"
+        />
+
+        {isAuthenticated ? (
+          <Text style={styles.positive}>SUCCESS</Text>
+        ) : (
+          <Text style={styles.negative}>FAILED</Text>
+        )}
+
+        {error ? (
+          <React.Fragment>
+            <View style={styles.errorBox}>
+              <Text>ERROR: {error}</Text>
+            </View>
+          </React.Fragment>
+        ) : null}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default BiometryScreen;
